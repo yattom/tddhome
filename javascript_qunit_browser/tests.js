d2dbe2318b34579c68afdf40b28c5cce19385037
Toshiyuki Ando <couger@kt.rim.or.jp>
2018-03-11 16:19:59 +0900

    add test player vs monster

--- javascript_qunit_browser/tests.js
+++ javascript_qunit_browser/tests.js
@@ -1,3 +1,12 @@
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
+
+QUnit.test( "プレイヤーがモンスターを攻撃する", function( assert ) {
+  var game = new RPG();
+  game.setPlayer(new Player());
+  game.setMonster(new Monster());
+  game.attack();
+  var msg = game.getLastMessage();
+  assert.ok( msg == "プレイヤーがモンスターを攻撃した", "Passed!" );
+});

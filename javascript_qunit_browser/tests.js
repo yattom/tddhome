7c29307d7f5140929583ce1281de9bc747491fa6
Toshiyuki Ando <couger@kt.rim.or.jp>
2018-03-11 17:07:48 +0900

    attack in turn

--- javascript_qunit_browser/tests.js
+++ javascript_qunit_browser/tests.js
@@ -1,23 +1,40 @@
 
 class RPG {
-    constructor() {
-        this.animaltype = "ポメラニアン"
-    }
+  constructor() {
+  }
 
-    setPlayer(player) {
-    }
-    setMonster(monster) {
-    }
-    attack(){
+  setPlayer(player) {
+    this.player = player
+    this.attacker = player
+  }
 
-    }
-    getLastMessage() {
-        return "プレイヤーがモンスターを攻撃した"
-    }
+  setMonster(monster) {
+    this.monster = monster
+    this.defender = monster
+  }
+
+  attack(){
+    this.lastMessage = this.attacker.name + "が" + this.defender.name + "を攻撃した"
+    var temp = this.attacker
+    this.attacker = this.defender
+    this.defender = temp
+  }
+
+  getLastMessage() {
+    return this.lastMessage
+  }
 }
 
-class Player {}
-class Monster {}
+class Player {
+  constructor() {
+    this.name = "プレイヤー"
+  }
+}
+class Monster {
+  constructor() {
+    this.name = "モンスター"
+  }
+}
 
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
@@ -32,5 +49,13 @@ QUnit.test( "プレイヤーがモンスターを攻撃する", function( assert
   assert.ok( msg == "プレイヤーがモンスターを攻撃した", "Passed!" );
 });
 
+
 QUnit.test( "プレイヤーの攻撃の後は、モンスターがプレイヤーを攻撃する", function( assert ) {
+  var game = new RPG();
+  game.setPlayer(new Player());
+  game.setMonster(new Monster());
+  game.attack();
+  game.attack();
+  var msg = game.getLastMessage();
+  assert.equal( msg, "モンスターがプレイヤーを攻撃した" );
 });

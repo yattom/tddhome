753ab7890f82efe0313e32507f9934030b6beb1e
yattom <tsutomu.yasui@gmail.com>
2018-03-11 16:01:23 +0900

    copy from repl.it

---
+++ javascript_qunit_browser/index.js
@@ -0,0 +1,1 @@
+let qunit = require('qunit');
--- javascript_qunit_browser/tests.js
+++ javascript_qunit_browser/tests.js
@@ -1,3 +1,3 @@
-QUnit.test( "失敗するテスト", function( assert ) {
-  assert.ok( 1 == 0, "失敗する" );
+QUnit.test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
 });

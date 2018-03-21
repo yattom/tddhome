5f023988cb5938af314c6e8a48abdc119fb66262
yattom <tsutomu.yasui@gmail.com>
2018-03-21 20:39:33 +0900

    add target

---
+++ C++_GoogleTest_VisualStudio2017/Target/target.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+#include "target.h"
+
+int foo(int v) {
+	return v * 2;
+}
--- C++_GoogleTest_VisualStudio2017/Test/test.cpp
+++ C++_GoogleTest_VisualStudio2017/Test/test.cpp
@@ -1,4 +1,9 @@
 #include "pch.h"
+#include "../Target/target.h"
+
+TEST(FooTest, FooTest) {
+	EXPECT_EQ(2, foo(1));
+}
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);

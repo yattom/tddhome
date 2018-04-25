bc56d4ed5f1b67785d21a9037691973d67bc11b6
yattom <tsutomu.yasui@gmail.com>
2018-04-25 11:36:23 +0900

    test for all items

--- javascript_mocha/test/test.js
+++ javascript_mocha/test/test.js
@@ -8,8 +8,17 @@ function assert_item(item, name, price) {
 }
 describe('defined items', function() {
   describe('has right name and price', function() {
-    it('for Apple', function() {
+    it('for 10 items', function() {
       assert_item(items[1], 'Apple', 100);
+      assert_item(items[2], 'Mikan', 40);
+      assert_item(items[3], 'Grape', 150);
+      assert_item(items[4], 'Nori Bento', 350);
+      assert_item(items[5], 'Shake Bento', 400);
+      assert_item(items[6], 'Tobacco', 420);
+      assert_item(items[7], 'Menthol Tobacco', 440);
+      assert_item(items[8], 'Lighter', 100);
+      assert_item(items[9], 'Tea', 80);
+      assert_item(items[10], 'Coffee', 100);
     });
   });
 });

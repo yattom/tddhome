e6607e3512d3ba38dda651e58b89acb0153294c7
yattom <tsutomu.yasui@gmail.com>
2018-04-25 14:36:59 +0900

    add all items

--- javascript_mocha/target.js
+++ javascript_mocha/target.js
@@ -1,3 +1,12 @@
 exports.items = {
   1: { name: 'Apple', price: 100 },
+  2: { name: 'Mikan', price: 40 },
+  3: { name: 'Grape', price: 150 },
+  4: { name: 'Nori Bento', price: 350 },
+  5: { name: 'Shake Bento', price: 400 },
+  6: { name: 'Tobacco', price: 420 },
+  7: { name: 'Menthol Tobacco', price: 440 },
+  8: { name: 'Lighter', price: 100 },
+  9: { name: 'Tea', price: 80 },
+  10: { name:  'Coffee', price: 100 },
 };

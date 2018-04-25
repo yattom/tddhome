fbaeb5940ab1e1814c9aad61715ae55328b656b9
yattom <tsutomu.yasui@gmail.com>
2018-04-25 11:34:49 +0900

    refactoring test

--- javascript_mocha/test/test.js
+++ javascript_mocha/test/test.js
@@ -2,15 +2,14 @@
 var assert = require('assert');
 var items = require('../target').items;
 
+function assert_item(item, name, price) {
+  assert.equal(item.name, name);
+  assert.equal(item.price, price);
+}
 describe('defined items', function() {
-  describe('has right name', function() {
+  describe('has right name and price', function() {
     it('for Apple', function() {
-      assert.equal(items[1].name, 'Apple');
-    });
-  });
-  describe('has right unit price', function() {
-    it('for Apple', function() {
-      assert.equal(items[1].price, 100);
+      assert_item(items[1], 'Apple', 100);
     });
   });
 });

e84954cc3eca9ad73f9739000b4961a4112ba1b3
yattom <tsutomu.yasui@gmail.com>
2018-06-07 10:17:06 +0900

    cosmetic

--- javascript_mocha/test/test.js
+++ javascript_mocha/test/test.js
@@ -1,14 +1,14 @@
 var assert = require('assert');
 var target = require('../target');
 
-describe('Calc', function() {
-  describe('#add()', function() {
-    it('should add 1 and 2', function() {
+describe('Calc', function () {
+  describe('#add()', function () {
+    it('should add 1 and 2', function () {
       assert.equal(3, target.Calc.add(1, 2));
     });
   });
-  describe('#mul()', function() {
-    it('should mul 1 and 2', function() {
+  describe('#mul()', function () {
+    it('should mul 1 and 2', function () {
       assert.equal(2, target.Calc.mul(1, 2));
     });
   });

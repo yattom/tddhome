34b283d70a01f683e371cacd113a3a51783e56a0
yattom <tsutomu.yasui@gmail.com>
2018-08-29 08:55:05 +0900

    reset to starting point

--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -1,30 +1,4 @@
 from foo.hello import *
 
-SMALLEST_PRIME = 2
-
-
 def test_hello():
     pass
-
-def is_dividable(target, prime):
-    return target % prime == 0
-
-def get_smallest_prime_factor(target):
-    prime = SMALLEST_PRIME
-    while prime <= target:
-        if is_dividable(target, prime):
-            target = int(target / prime)
-            return prime, target
-            break
-        prime += 1
-
-
-def prime_factoring(target):
-    prime_factors = []
-    while target > 1:
-        prime, target = get_smallest_prime_factor(target)
-        prime_factors.append(prime)
-    return prime_factors
-
-def test_素因数分解():
-    assert prime_factoring(100) == [2, 2, 5, 5]

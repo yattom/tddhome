205f0c8f2a298623a1f14f48745dba9364386b59
yattom <tsutomu.yasui@gmail.com>
2018-09-05 16:03:36 +0900

    fizzbuzz works

--- python_pytest/foo/target.py
+++ python_pytest/foo/target.py
@@ -1,2 +1,8 @@
-def func_42():
-    return 64
+def fizzbuzz(i):
+    if i % 15 == 0:
+        return "FizzBuzz"
+    if i % 3 == 0:
+        return "Fizz"
+    if i % 5 == 0:
+        return "Buzz"
+    return str(i)
--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -1,4 +1,23 @@
-from foo import target
+import pytest
 
-def test_func_42():
-    assert target.func_42() == 42
+from foo.target import fizzbuzz
+
+@pytest.mark.parametrize('i,s', [(1, "1")])
+def test_数をそのまま文字列にする(i, s):
+    assert fizzbuzz(i) == s
+
+@pytest.mark.parametrize('i', [3])
+def test_3の倍数はFizzにする(i):
+    assert fizzbuzz(i) == "Fizz"
+
+@pytest.mark.parametrize('i', [5])
+def test_5の倍数はBuzzにする(i):
+    assert fizzbuzz(i) == "Buzz"
+
+@pytest.mark.parametrize('i', [15])
+def test_3と5の倍数はFizzBuzzにする(i):
+    assert fizzbuzz(i) == "FizzBuzz"
+
+def test_1から100まで():
+    assert count == 100
+

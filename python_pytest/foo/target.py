f3d49472ca5422a176b409d99587f43eec3b6f2c
yattom <tsutomu.yasui@gmail.com>
2018-09-06 17:30:30 +0900

    Revert "fizzbuzz works"
    This reverts commit 205f0c8f2a298623a1f14f48745dba9364386b59.

--- python_pytest/foo/target.py
+++ python_pytest/foo/target.py
@@ -1,8 +1,2 @@
-def fizzbuzz(i):
-    if i % 15 == 0:
-        return "FizzBuzz"
-    if i % 3 == 0:
-        return "Fizz"
-    if i % 5 == 0:
-        return "Buzz"
-    return str(i)
+def func_42():
+    return 64
--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -1,23 +1,4 @@
-import pytest
+from foo import target
 
-from foo.target import fizzbuzz
-
-@pytest.mark.parametrize('i,s', [(1, "1")])
-def test_数をそのまま文字列にする(i, s):
-    assert fizzbuzz(i) == s
-
-@pytest.mark.parametrize('i', [3])
-def test_3の倍数はFizzにする(i):
-    assert fizzbuzz(i) == "Fizz"
-
-@pytest.mark.parametrize('i', [5])
-def test_5の倍数はBuzzにする(i):
-    assert fizzbuzz(i) == "Buzz"
-
-@pytest.mark.parametrize('i', [15])
-def test_3と5の倍数はFizzBuzzにする(i):
-    assert fizzbuzz(i) == "FizzBuzz"
-
-def test_1から100まで():
-    assert count == 100
-
+def test_func_42():
+    assert target.func_42() == 42

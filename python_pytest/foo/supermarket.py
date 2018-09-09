b816dced00be616b594cd5bd966a35ea416a1f69
yattom <tsutomu.yasui@gmail.com>
2018-09-09 12:25:59 +0900

    お題1

--- python_pytest/foo/supermarket.py
+++ python_pytest/foo/supermarket.py
@@ -1,3 +1,27 @@
+ITEMS = {
+    1: 100,
+    2: 40,
+    3: 150,
+    4: 350,
+    5: 400,
+    6: 420,
+    7: 440,
+    8: 100,
+    9: 80,
+    10: 100,
+}
+
 class Cart:
+    def __init__(self):
+        self.contents = []
+
+    def add_item(self, item_id, amount):
+        if item_id not in ITEMS:
+            raise ValueError(f"invalid item_id {item_id}")
+        if type(amount) != int or amount <= 0:
+            raise ValueError(f"invalid amount {amount}")
+        self.contents.append((item_id, amount))
+
     def total(self):
-        return 0
+        return sum([ITEMS[item_id] * amount for item_id, amount in self.contents])
+
--- python_pytest/test/supermarket_test.py
+++ python_pytest/test/supermarket_test.py
@@ -2,7 +2,25 @@ import pytest
 
 from foo.supermarket import *
 
-def test_cart_empty():
-    cart = Cart()
-    assert cart.total() == 0
+@pytest.fixture
+def cart():
+    return Cart()
 
+def test_checkout_items_りんご2個とのり弁1個で550円(cart):
+    cart.add_item(1, 2)
+    cart.add_item(4, 1)
+    assert cart.total() == 550
+
+class TestCart:
+    def test_最初は空(self, cart):
+        assert cart.total() == 0
+
+    def test_存在しない商品IDなら例外(self, cart):
+        cart = Cart()
+        with pytest.raises(ValueError):
+            cart.add_item(999, 1)
+
+    def test_個数は自然数なら例外(self, cart):
+        cart = Cart()
+        with pytest.raises(ValueError):
+            cart.add_item(1, 0)

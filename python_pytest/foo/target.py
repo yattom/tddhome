9e9b723387e9ff2afc9e05050069a0b2284077e0
yattom <tsutomu.yasui@gmail.com>
2018-09-13 14:20:27 +0900

    お題2

--- python_pytest/foo/target.py
+++ python_pytest/foo/target.py
@@ -1,8 +1,13 @@
 class VendingMachine:
     cup = None
+    coin = 0
 
     def push(self):
-        self.cup = 'コーラ'
+        if self.coin == 100:
+            self.cup = 'コーラ'
 
     def get_cup(self):
         return self.cup
+
+    def insert_100(self):
+        self.coin = 100
--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -5,9 +5,15 @@ from foo.target import *
 def vm():
     return VendingMachine()
 
-def test_ボタンを押すとコーラが出る(vm):
+def test_100円コインを投入しないでボタンを押してもコーラは出ない(vm):
     vm.push()
-    assert vm.get_cup() == 'コーラ'
+    assert vm.get_cup() == None
 
 def test_ボタンを押さないととコーラが出ない(vm):
     assert vm.get_cup() == None
+
+def test_100円コインを投入してからボタンを押すとコーラが出る(vm):
+    vm.insert_100()
+    vm.push()
+    assert vm.get_cup() == 'コーラ'
+

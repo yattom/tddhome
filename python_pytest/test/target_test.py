be5c10e7d2c215a3c1bd2e946ab602f71e8eaa3c
yattom <tsutomu.yasui@gmail.com>
2018-09-13 14:13:33 +0900

    お題1 リファクタリング

--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -1,10 +1,13 @@
+import pytest
 from foo.target import *
 
-def test_ボタンを押すとコーラが出る():
-    sut = VendingMachine()
-    sut.push()
-    assert sut.get_cup() == 'コーラ'
+@pytest.fixture
+def vm():
+    return VendingMachine()
 
-def test_ボタンを押さないととコーラが出ない():
-    sut = VendingMachine()
-    assert sut.get_cup() == None
+def test_ボタンを押すとコーラが出る(vm):
+    vm.push()
+    assert vm.get_cup() == 'コーラ'
+
+def test_ボタンを押さないととコーラが出ない(vm):
+    assert vm.get_cup() == None

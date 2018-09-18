78ceb4e3b12952252851f103b8d8f0845ebd5180
yattom <tsutomu.yasui@gmail.com>
2018-09-18 12:11:47 +0900

    done vendingmachine

--- python_pytest/foo/target.py
+++ python_pytest/foo/target.py
@@ -1,9 +1,27 @@
 class VendingMachine:
     def __init__(self):
         self._cup = None
+        self._coin = 0
+        self._buttons = [
+            Button(self, 'コーラ'),
+            Button(self, 'ウーロン茶'),
+        ]
 
     def get_from_cup(self):
         return self._cup
 
+    def insert100(self):
+        self._coin = 100
+
+    def select_button(self, idx):
+        return self._buttons[idx]
+
+
+class Button:
+    def __init__(self, vm, beverage):
+        self._vm = vm
+        self._bevarage = beverage
+
     def push(self):
-        self._cup = 'コーラ'
+        if self._vm._coin == 100:
+            self._vm._cup = self._bevarage
--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -1,12 +1,24 @@
 from foo.target import *
+import pytest
 
-def test_ボタンを押すとコーラが出る():
-    vm = VendingMachine()
-    vm.push()
-    actual = vm.get_from_cup()
-    assert actual == 'コーラ'
+@pytest.fixture
+def vm():
+    # 前処理
+    yield VendingMachine()
+    # 後処理
 
-def test_ボタンを押さないとコーラが出ない():
-    vm = VendingMachine()
-    actual = vm.get_from_cup()
-    assert actual == None
+def test_100円入れずにボタンを押すとコーラが出ない(vm):
+    vm.select_button(0).push()
+    assert vm.get_from_cup() == None
+
+def test_ボタンを押さないとコーラが出ない(vm):
+    assert vm.get_from_cup() == None
+
+@pytest.mark.parametrize('idx,bevarage', [
+    (0, 'コーラ'),
+    (1, 'ウーロン茶'),
+])
+def test_押したボタンに応じて飲み物が出る(vm, idx, bevarage):
+    vm.insert100()
+    vm.select_button(idx).push()
+    assert vm.get_from_cup() == bevarage

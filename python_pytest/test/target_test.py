d94bd3db60adc9caef3dc04c3098ee936d2906db
yattom <tsutomu.yasui@gmail.com>
2018-09-18 12:57:27 +0900

    playing with vendingmachine

--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -22,3 +22,20 @@ def test_押したボタンに応じて飲み物が出る(vm, idx, bevarage):
     vm.insert100()
     vm.select_button(idx).push()
     assert vm.get_from_cup() == bevarage
+
+def test_200円入れるとレッドブルが出る(vm):
+    vm.insert100()
+    vm.insert100()
+    vm.select_button(2).push()
+    assert vm.get_from_cup() == 'レッドブル'
+
+def test_100円入れるとレッドブルが出ない(vm):
+    vm.insert100()
+    vm.select_button(2).push()
+    assert vm.get_from_cup() == None
+
+def test_100円でコーラを買うと残額は0円(vm):
+    vm.insert100()
+    vm.select_button(0).push()
+    assert vm.get_from_cup() == 'コーラ'
+    assert vm.coin_remain() == 0

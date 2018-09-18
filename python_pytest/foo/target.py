6c10f6ffbb66414accf2a81c115beaec25c1797d
yattom <tsutomu.yasui@gmail.com>
2018-09-18 13:58:41 +0900

    init for fizzbuzz

--- python_pytest/foo/target.py
+++ python_pytest/foo/target.py
@@ -1,13 +1,2 @@
-def fizzbuzz(num):
-    if num % 15 == 0:
-        return 'FizzBuzz'
-    if num % 3 == 0:
-        return 'Fizz'
-    if num % 5 == 0:
-        return 'Buzz'
-    return str(num)
-
-def print_fizbuzz(num):
-    for i in range(1, num + 1):
-        print(fizzbuzz(i))
-
+def func_42():
+    return 64

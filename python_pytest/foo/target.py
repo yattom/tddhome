8a0b6c4252b29ec6714c2659026ed1b47cfdd127
yattom <tsutomu.yasui@gmail.com>
2018-09-18 13:31:43 +0900

    done fizzbuzz

--- python_pytest/foo/target.py
+++ python_pytest/foo/target.py
@@ -1,2 +1,13 @@
-def func_42():
-    return 64
+def fizzbuzz(num):
+    if num % 15 == 0:
+        return 'FizzBuzz'
+    if num % 3 == 0:
+        return 'Fizz'
+    if num % 5 == 0:
+        return 'Buzz'
+    return str(num)
+
+def print_fizbuzz(num):
+    for i in range(1, num + 1):
+        print(fizzbuzz(i))
+
--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -1,5 +1,32 @@
 from foo.target import *
 import pytest
 
-def test_42():
-    assert func_42() == 42
+class Test数はそのまま:
+    def test_数1をそのまま(self):
+        assert fizzbuzz(1) == "1"
+
+    def test_数2をそのまま(self):
+        assert fizzbuzz(2) == "2"
+
+@pytest.mark.parametrize('num', [3, 6])
+def test_ただし3の倍数のときはFizzにする(num):
+    assert fizzbuzz(num) == "Fizz"
+
+def test_5の倍数のときはBuzzにする():
+    assert fizzbuzz(5) == "Buzz"
+
+def test_3と5両方の倍数の場合にはFizzBuzzにする():
+    assert fizzbuzz(15) == "FizzBuzz"
+
+class Test指定した数まで実行する:
+    def test_1から5までプリントする(self, capsys):
+        print_fizbuzz(5)
+        actual, _ = capsys.readouterr()
+        assert actual == "1\n2\nFizz\n4\nBuzz\n"
+
+    def test_1から100までプリントする(self, capsys):
+        print_fizbuzz(100)
+        actual, _ = capsys.readouterr()
+        actual = actual.split('\n')
+        assert 100 + 1 == len(actual)
+

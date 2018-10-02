de649a539b53601aec302331fdff5fa45f5fdc11
yattom <tsutomu.yasui@gmail.com>
2018-10-02 17:03:31 +0900

    test pass

--- python_pytest_web/tests/test_say_hello.py
+++ python_pytest_web/tests/test_say_hello.py
@@ -5,10 +5,10 @@ def test_say_hello():
     pass
 
 @when('I visit hello page')
-def visit_hello_page():
-    pass
+def visit_hello_page(browser):
+    browser.visit('http://localhost:5000/')
 
 @then('I shoud see Hello World')
-def see_hello_world():
-    pass
+def see_hello_world(browser):
+    assert browser.is_text_present('Hello World')
 

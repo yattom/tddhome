d11b1d4eead0cbbb5aaf78db61794b02b3e5f86b
yattom <tsutomu.yasui@gmail.com>
2018-10-02 16:09:44 +0900

    create project

---
+++ python_pytest_web/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="tddhome_python_pytest_web",
+    version="0.0.1",
+    author="Yattom",
+    author_email="tsutomu.yasui@gmail.com",
+    description="easy to start tdd home for python/pytest",
+    # packages=setuptools.find_packages(),
+    py_modules=['webapp'],
+)

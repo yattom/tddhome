82d2d11944c3eb5825c64c0e84475843872bd7da
yattom <tsutomu.yasui@gmail.com>
2018-10-02 17:45:24 +0900

    autostart webapp

--- python_pytest_web/tests/test_say_hello.py
+++ python_pytest_web/tests/test_say_hello.py
@@ -1,4 +1,21 @@
+import pytest
 from pytest_bdd import scenario, given, when, then
+
+import webapp.hello
+from webapp.hello import create_app
+
+import subprocess
+import time
+import os
+
+@pytest.fixture(autouse=True, scope='session')
+def webapp():
+    new_env = os.environ.copy()
+    new_env['FLASK_APP'] = 'webapp.hello'
+    server = subprocess.Popen(['flask', 'run'], env=new_env)
+    time.sleep(1)
+    yield server
+    server.terminate()
 
 @scenario('hello.feature', 'Say hello')
 def test_say_hello():
--- python_pytest_web/webapp/hello.py
+++ python_pytest_web/webapp/hello.py
@@ -1,6 +1,11 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def hello():
+        return "Hello World!"
+
+    return app
+

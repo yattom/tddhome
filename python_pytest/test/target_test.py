607a133bd88a9a0d5aab9846a39feb9746cea546
yattom <tsutomu.yasui@gmail.com>
2019-01-24 12:16:24 +0900

    finished

--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -1,4 +1,17 @@
 from foo import target
 
+class Cell:
+    def __init__(self, status):
+        self.status = status
+
+    def next_generation(self):
+        return Cell("DEAD")
+
 def test_func_42():
     assert target.func_42() == 42
+
+def test_死んでいるセルに隣接する生きたセルがちょうど3つあれば次の世代が誕生する():
+    cell = Cell("DEAD")
+    next_cell = cell.next_generation()
+    assert next_cell.status == "DEAD"
+

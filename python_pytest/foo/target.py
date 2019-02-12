e3ab6ad29c832dbddcea504c284ce6b48c155edb
yattom <tsutomu.yasui@gmail.com>
2019-02-12 09:58:38 +0900

    雑に進めた

--- python_pytest/foo/target.py
+++ python_pytest/foo/target.py
@@ -1,2 +1,31 @@
-def func_42():
-    return 64
+import math
+
+REDUCED_RATE_APPLICABLE_ITEMS = [
+    "オロナミンC",
+    "手巻直火焼き紅しゃけ",
+]
+
+PRICE_LIST = {
+    "手巻直火焼き紅しゃけ": 139,
+    "キリンチューハイ氷結グレープフルーツ350ml缶": 141,
+}
+
+TAX_RATE = 1.10
+TAX_RATE_REDUCED = 1.08
+
+def is_reduced_rate_applicable(item):
+    return item in REDUCED_RATE_APPLICABLE_ITEMS
+
+def base_price(item):
+    return PRICE_LIST[item]
+
+def tax_included(items):
+    total = 0
+    total_reduced = 0
+    for item, count in items:
+        if is_reduced_rate_applicable(item):
+            total_reduced += base_price(item) * count
+        else:
+            total += base_price(item) * count
+
+    return int(math.floor(total_reduced * TAX_RATE_REDUCED + total * TAX_RATE))
--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -1,4 +1,28 @@
-from foo import target
+from foo.target import is_reduced_rate_applicable, tax_included, base_price
 
-def test_func_42():
-    assert target.func_42() == 42
+def test_オロナミンCは軽減税率対象():
+    item = "オロナミンC"
+    assert is_reduced_rate_applicable(item)
+
+def test_リポビタンDは軽減税率対象外():
+    item = "リポビタンD"
+    assert not is_reduced_rate_applicable(item)
+
+def test_手巻直火焼き紅しゃけは軽減税率対象():
+    item = "手巻直火焼き紅しゃけ"
+    assert is_reduced_rate_applicable(item)
+
+def test_おにぎりの税抜価格():
+    assert base_price("手巻直火焼き紅しゃけ") == 139
+
+class Test合計金額を求める:
+    def test_おにぎり2個(self):
+        total = tax_included([("手巻直火焼き紅しゃけ", 2)])
+        assert 300 == total
+
+    def test_軽減対象と対象外の組み合わせ(self):
+        total = tax_included([
+            ("手巻直火焼き紅しゃけ", 2),
+            ("キリンチューハイ氷結グレープフルーツ350ml缶", 3),
+        ])
+        assert 765 == total

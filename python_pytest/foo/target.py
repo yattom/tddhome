485e6f618eead709bfda8817f2fa2e416a24b7bf
yattom <tsutomu.yasui@gmail.com>
2019-06-11 10:59:32 +0900

    initial - no test

--- python_pytest/foo/target.py
+++ python_pytest/foo/target.py
@@ -1,2 +1,13 @@
-def func_42():
-    return 64
+def is_adult(age, sex=None, type=None):
+    if type is None:
+        return 20 <= age
+    if type == "Marriage":
+        if sex == "M":
+            limit = 18
+        elif sex == "F":
+            limit = 16
+        else:
+            raise ValueError()
+        return limit <= age
+    _, low, high = list(filter(lambda e: e[0] == type, [("Entry", 13, 65)]))[0]
+    return low <= age <= high
--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -1,4 +1,6 @@
 from foo import target
 
-def test_func_42():
-    assert target.func_42() == 42
+def test_nothing():
+    assert 1 == 2
+
+

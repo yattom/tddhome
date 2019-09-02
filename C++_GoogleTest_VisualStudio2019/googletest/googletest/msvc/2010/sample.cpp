042548ac6f7a326de896914fe411a3f17c421b10
yattom <tsutomu.yasui@gmail.com>
2019-09-02 15:19:17 +0900

    C++/GoogleTest/VS2019

---
+++ C++_GoogleTest_VisualStudio2019/googletest/googletest/msvc/2010/sample.cpp
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "sample.h"
+
+const char* hello() {
+	return "Hello, World!\n";
+}
+
---
+++ C++_GoogleTest_VisualStudio2019/googletest/newsample/sample_unittest.cpp
@@ -0,0 +1,10 @@
+#include <limits.h>
+#include "sample.h"
+#include "../googletest/include/gtest/gtest.h"
+namespace {
+
+	TEST(FactorialTest, Negative) {
+		EXPECT_EQ(1, 0);
+	}
+
+}

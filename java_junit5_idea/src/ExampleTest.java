090c7d7507c56e898b868d593013a10d0ab56bb9
yattom <tsutomu.yasui@gmail.com>
2019-09-03 10:12:29 +0900

    implement HelloWorld

--- java_junit5_idea/src/ExampleTest.java
+++ java_junit5_idea/src/ExampleTest.java
@@ -4,13 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ExampleTest {
     @Test
     public void 失敗するテスト() {
-        assertEquals("expected", "expected", "成功する");
+        assertEquals("expected", "expectedXXX", "成功する");
     }
 
     @Test
     public void HelloWorldテスト() {
         var sut = new HelloWorld();
         String actual = sut.say();
-        assertEquals("Hello World!", actual);
+        assertEquals("Hello World!XXX", actual, "HelloWorldを出力する");
     }
 }
--- java_junit5_idea/src/HelloWorld.java
+++ java_junit5_idea/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public String say() {
-        return null;
+        return "Hello World!";
     }
 }

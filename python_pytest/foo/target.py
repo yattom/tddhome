791b2f3d170fcfbecc2645a92b4892b9494eb769
yattom <tsutomu.yasui@gmail.com>
2019-11-19 10:12:37 +0900

    unit price

--- python_pytest/foo/target.py
+++ python_pytest/foo/target.py
@@ -1,2 +1,9 @@
-def func_42():
-    return 64
+def get_unit_price(item_no):
+    if item_no == 1:
+        return 100
+    if item_no == 2:
+        return 40
+    if item_no == 3:
+        return 150
+    return 100
+
--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -1,4 +1,11 @@
-from foo import target
+from foo.target import get_unit_price
 
-def test_func_42():
-    assert target.func_42() == 42
+def test_商品番号1から単価100を取り出す():
+    assert get_unit_price(1) == 100
+
+def test_商品番号2から単価40を取り出す():
+    assert get_unit_price(2) == 40
+
+def test_商品番号3から単価150を取り出す():
+    assert get_unit_price(3) == 150
+

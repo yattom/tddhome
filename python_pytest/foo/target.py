74cd137d0772032ec99f28153b73f0441a6b6dd6
yattom <tsutomu.yasui@gmail.com>
2019-11-19 13:25:52 +0900

    organize tests

--- python_pytest/foo/target.py
+++ python_pytest/foo/target.py
@@ -1,9 +1,33 @@
-def get_unit_price(item_no):
-    if item_no == 1:
-        return 100
-    if item_no == 2:
-        return 40
-    if item_no == 3:
-        return 150
-    return 100
+from dataclasses import dataclass
 
+@dataclass
+class Item:
+    item_no: int
+    unit_price: int
+    tax_included: bool = False
+
+ITEMS = {
+    1: Item(1, 100),
+    2: Item(2, 40),
+    3: Item(3, 150),
+    4: Item(4, 150),
+    5: Item(5, 400),
+    6: Item(6, 420, True),
+    7: Item(7, 440, True),
+    8: Item(8, 100),
+    9: Item(9, 80),
+    10: Item(10, 100),
+}
+
+def get_unit_price(item_no):
+    return ITEMS[item_no].unit_price
+
+def get_subtotal_of_item(items):
+    return sum([ITEMS[item[0]].unit_price * item[1] for item in items])
+
+def get_subtotal_of_item_incl_tax(items):
+    return int(sum([
+        ITEMS[item[0]].unit_price
+         * (item[1] - (item[1] // 10))
+         * (1.00 if ITEMS[item[0]].tax_included else 1.08)
+        for item in items]))
--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -1,11 +1,34 @@
-from foo.target import get_unit_price
+from foo.target import *
+import pytest
 
-def test_商品番号1から単価100を取り出す():
-    assert get_unit_price(1) == 100
+class Testスーパーの支払金額:
+    @pytest.mark.parametrize('item_no, unit_price',
+        [(1, 100), (2, 40), (3, 150), (4, 150),
+         (5, 400), (6, 420), (7, 440), (8, 100),
+         (9, 80), (10, 100)])
+    def test_商品1個の単価を取り出す(self, item_no, unit_price):
+        assert get_unit_price(item_no) == unit_price
 
-def test_商品番号2から単価40を取り出す():
-    assert get_unit_price(2) == 40
+    class Test複数の商品の合計金額:
+        def test_1個だけの場合(self):
+            assert get_subtotal_of_item([(1, 1)]) == 100
 
-def test_商品番号3から単価150を取り出す():
-    assert get_unit_price(3) == 150
+        def test_1商品複数個の場合(self):
+            assert get_subtotal_of_item([(1, 3)]) == 300
+
+        def test_2商品1個ずつの場合(self):
+            assert get_subtotal_of_item([(1, 1), (4, 1)]) == 250
+
+        def test_2商品複数個ずつの場合(self):
+            assert get_subtotal_of_item([(8, 8), (5, 5)]) == 2800
+
+    class Testタバコの取り扱い:
+        def test_タバコは内税(self):
+            assert get_subtotal_of_item_incl_tax([(6, 1)]) == 420
+
+        def test_タバコ10個で1個おまけ_11個の場合(self):
+            assert get_subtotal_of_item_incl_tax([(6, 11)]) == 420 * 10
+
+        def test_タバコ10個で1個おまけ_25個の場合(self):
+            assert get_subtotal_of_item_incl_tax([(6, 25)]) == 420 * (25 - 2)
 

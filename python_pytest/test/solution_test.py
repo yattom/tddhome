161474a62f50dbbe59a18742bf7e9f1e8e6494ee
yattom <tsutomu.yasui@gmail.com>
2019-12-17 12:18:31 +0900

    retry contest 15

--- python_pytest/test/solution_test.py
+++ python_pytest/test/solution_test.py
@@ -148,43 +148,6 @@ class Solution:
 
         return len(ranges) - len(to_remove)
 
-    def minFallingPathSum(self, arr: List[List[int]]) -> int:
-        width = len(arr[0])
-        def advance(picking):
-            for idx in range(width):
-                picking[idx] += 1
-                if picking[idx] >= width:
-                    if idx == width - 1:
-                        raise StopIteration
-                    picking[idx] = 0
-                else:
-                    break
-
-        def validate(picking):
-            while True:
-                invalid = False
-                for i, v in enumerate(picking):
-                    if (i > 0 and picking[i - 1] == v) or (i < len(picking) - 1 and picking[i + 1] == v):
-                        invalid = True
-                if not invalid:
-                    return
-                advance(picking)
-
-
-        def cols():
-            picking = ([0, 1] * width)[:width]
-            while True:
-                try:
-                    advance(picking)
-                    validate(picking)
-                    # print(picking)
-                    yield picking
-                except StopIteration:
-                    return
-
-        values = [sum([arr[row][col] for row, col in enumerate(picking)]) for picking in cols()]
-
-        return min(values)
 
 class CombinationIterator:
 
@@ -303,10 +266,3 @@ def test_CombinationIterator_1():
         if not obj.hasNext():
             break
 
-
-@pytest.mark.parametrize("arr, answer", [
-    ([[1,2,3],[4,5,6],[7,8,9]], 13),
-    ([[-2,-18,31,-10,-71,82,47,56,-14,42],[-95,3,65,-7,64,75,-51,97,-66,-28],[36,3,-62,38,15,51,-58,-90,-23,-63],[58,-26,-42,-66,21,99,-94,-95,-90,89],[83,-66,-42,-45,43,85,51,-86,65,-39],[56,9,9,95,-56,-77,-2,20,78,17],[78,-13,-55,55,-7,43,-98,-89,38,90],[32,44,-47,81,-1,-55,-5,16,-81,17],[-87,82,2,86,-88,-58,-91,-79,44,-9],[-96,-14,-52,-8,12,38,84,77,-51,52]], 0),
-])
-def test_minFallingPathSum(sut, arr, answer):
-    assert sut.minFallingPathSum(arr) == answer

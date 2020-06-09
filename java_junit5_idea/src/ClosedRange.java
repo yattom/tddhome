5d0eee8bccaa319e7e567085b54382a0b7c7e24a
yattom <tsutomu.yasui@gmail.com>
2020-06-09 18:13:54 +0900

    end of session

--- java_junit5_idea/src/ClosedRange.java
+++ java_junit5_idea/src/ClosedRange.java
@@ -1,2 +1,13 @@
 public class ClosedRange {
+    private final int higher;
+    private final int lower;
+
+    public ClosedRange(int lower, int higher) {
+        this.lower = lower;
+        this.higher = higher;
+    }
+
+    public String toString() {
+        return "[" + lower + "," + higher + "]";
+    }
 }
--- java_junit5_idea/src/ClosedRangeTest.java
+++ java_junit5_idea/src/ClosedRangeTest.java
@@ -1,10 +1,30 @@
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ClosedRangeTest {
-    @Test
-    public void 文字列表現が3_8() {
-        String actual = new ClosedRange().toString();
-        assertEquals("[3,8]", actual);
+    @Nested
+    public class 文字列表現 {
+        @Test
+        public void 小さな整数() {
+            String actual = new ClosedRange(3, 8).toString();
+            assertEquals("[3,8]", actual);
+        }
+
+        @Test
+        public void 負の数() {
+            String actual = new ClosedRange(-22, -9).toString();
+            assertEquals("[-22,-9]", actual);
+        }
+
+        @Test
+        public void 整数の限界() {
+            String actual = new ClosedRange(
+                    Integer.MIN_VALUE,
+                    Integer.MAX_VALUE).toString();
+            assertEquals("[-2147483648,2147483647]", actual);
+        }
     }
+
+
 }

c56fb06e32736c0c07f7807ecdb737a769e18c77
yattom <tsutomu.yasui@gmail.com>
2021-03-22 13:57:25 +0900

    add CSharp example

---
+++ csharp_xunit/VendingMachine/VendingMachineTest/VendingMachineTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace VendingMachineTest
+{
+    public class VendingMachineTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

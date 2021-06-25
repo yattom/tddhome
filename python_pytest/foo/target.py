652802f38852361571ed98b84736ca678a2b7602
yattom <tsutomu.yasui@gmail.com>
2021-06-25 19:56:48 +0900

    finished

--- python_pytest/foo/target.py
+++ python_pytest/foo/target.py
@@ -1,2 +1,13 @@
-def func_42():
-    return 64
+class VendingMachine:
+    def __init__(self):
+        self._coin = 0
+    def push(self):
+        if self._coin == 100:
+            return 'コーラ'
+        return None
+
+    def get_coins(self):
+        return self._coin
+
+    def deposit(self, coin):
+        self._coin = coin
--- python_pytest/test/target_test.py
+++ python_pytest/test/target_test.py
@@ -1,4 +1,42 @@
 from foo import target
+from foo.target import VendingMachine
 
-def test_func_42():
-    assert target.func_42() == 42
+import pytest
+
+@pytest.fixture
+def vending_machine():
+    return VendingMachine()
+
+
+class Testボタンを押す:
+    def test_100円投入せずにボタンを押すと何も出ない(self, vending_machine):
+        # 前提
+        # 実行
+        actual = vending_machine.push()
+        # 検証
+        assert actual == None
+
+    def test_100円投入してからボタンを押すとコーラが出る(self, vending_machine):
+        # 前提
+        vending_machine.deposit(100)
+        # 実行
+        actual = vending_machine.push()
+        # 検証
+        assert actual == 'コーラ'
+
+
+class Testお金を投入できる:
+    def test_最初は0円(self, vending_machine):
+        # 前提
+        # 実行
+        # 検証
+        assert vending_machine.get_coins() == 0
+
+    def test_100円投入したら残高は100(self, vending_machine):
+        # 前提
+        vending_machine.deposit(100)
+        # 実行
+        # 検証
+        assert vending_machine.get_coins() == 100
+
+

dd4b40e310becc92f75f52d4c6ca59c13084ced2
yattom <tsutomu.yasui@gmail.com>
2023-12-05 13:25:05 +0900

    fix python test

--- python_pytest/foo/target.py
+++ python_pytest/foo/target.py
@@ -1,17 +1,2 @@
-def confirm_age(age):
-    if age < 18:
-        return "未成年"
-    if age < 20:
-        return "成年 (飲酒不可)"
-    else:
-        return "成年"
-    return "未定義"
-
-
-def calc_ticket(age):
-    if age < 6:
-        return 0
-    if age < 12:
-        return 800
-    return 1600
-
+def answer():
+    return 7 * 8

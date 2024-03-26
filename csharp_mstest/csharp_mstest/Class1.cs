1476a29fc48fa06f11e633aec00efd720ed9f391
yattom <tsutomu.yasui@gmail.com>
2024-03-26 09:05:40 +0900

    up to date

---
+++ csharp_mstest/csharp_mstest/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace csharp_mstest
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ csharp_mstest/csharp_mstest_test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace csharp_mstest
+{ 
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var class1 = new Class1();
+        }
+    }
+}
